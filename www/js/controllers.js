115bb389b89ceaaeaf5ddee0d8b4ebc124e91c13
zohaibmir <zohaib.mir@gmail.com>
2015-09-08 23:43:56 +0200

    update profile and signup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,7 @@ angular.module('starter.controllers', [])
 
             $scope.signUpData = {};
             // Create the login modal that we will use later
-            $ionicModal.fromTemplateUrl('templates/signup.html', {
+            $ionicModal.fromTemplateUrl('templates/profile.html', {
                 scope: $scope
             }).then(function (modal) {
                 $scope.modal = modal;
@@ -144,14 +144,16 @@ angular.module('starter.controllers', [])
                 $scope.modal.show();
             };
 
+            
             // Perform the login action when the user submits the login form
-            $scope.doLogin = function () {
+            $scope.SignUp = function () {
                 console.log('Doing login', $scope.loginData);
-
+                
                 // Simulate a login delay. Remove this and replace with your login
                 // code if using a login system
                 $timeout(function () {
                     $scope.closeLogin();
+                    $state.go('app.main');
                 }, 1000);
             };
         })
